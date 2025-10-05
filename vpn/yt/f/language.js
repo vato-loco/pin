d0bd33048657a84f1b2e1b50b6a1f6467cea8538
kingofpops <mwkt83@gmail.com>
2025-10-05 22:31:21 +0200

    VPN YT LP

---
+++ vpn/yt/f/language.js
@@ -0,0 +1,110 @@
+var lang = {};
+lang['english'] = {
+  a1: "(1) Notification",
+  a2: "VPN Update",
+  a3: "Please download VPN App to continue watching in safe mode",
+  a4: "Install",
+},
+lang['japan'] = {
+  a1: "(1) お知らせ",
+  a2: "VPN 更新",
+  a3: "セーフモードで視聴を続けるには、VPNアプリをダウンロードしてください",
+  a4: "インストール",
+},
+lang['portugal'] = {
+  a1: "(1) Notificação",
+     a2: "Atualização VPN",
+     a3: "Baixe o aplicativo VPN para continuar assistindo no modo de segurança",
+     a4: "Instalar",
+},
+lang['korea'] = {
+  a1 : "(1) 통지",
+     a2 : "VPN 업데이트",
+     a3 : "안전 모드에서 계속 시청하려면 VPN 앱을 다운로드하세요.",
+     a4 : "설치",
+},
+lang['thai'] = {
+  a1: "การแจ้งเตือน (1)",
+     a2: "อัพเดต VPN",
+     a3: "โปรดดาวน์โหลดแอป VPN เพื่อรับชมต่อในเซฟโหมด",
+     a4: "ติดตั้ง",
+},
+lang['indonasia'] = {
+  a1: "(1) Pemberitahuan",
+   a2: "Pembaruan VPN",
+   a3: "Silakan unduh Aplikasi VPN untuk terus menonton dalam mode aman",
+   a4: "Instal",
+},
+lang['france'] = {
+  a1: "(1) Notification",
+   a2: "Mise à jour VPN",
+   a3: "Veuillez télécharger l'application VPN pour continuer à regarder en mode sans échec",
+   a4: "Installer",
+},
+lang['spain'] = {
+  a1: "(1) Notificación",
+   a2: "Actualización de VPN",
+   a3: "Descargue la aplicación VPN para seguir viendo en modo seguro",
+   a4: "instalar",
+},
+lang['germany'] = {
+  a1: "(1) Benachrichtigung",
+     a2: "VPN Update",
+     a3: "Bitte laden Sie die VPN-App herunter, um im abgesicherten Modus weiterzusehen",
+     a4: "Installieren",
+},
+lang['norway'] = {
+  a1: "(1) Varsling",
+     a2: "VPN Update",
+     a3: "Last ned VPN-appen for å fortsette å se i sikker modus",
+     a4: "Installer",
+},
+lang['dutch'] = {
+  a1: "(1) Kennisgeving",
+   a2: "VPN-update",
+   a3: "Download de VPN-app om verder te kijken in de veilige modus",
+   a4: "Installeren",
+}
+
+lang['italy'] = {
+  a1: "(1) Notifica",
+   a2: "Aggiornamento VPN",
+   a3: "Scarica l'app VPN per continuare a guardare in modalità provvisoria",
+   a4: "Installa",
+},
+lang['sweden'] = {
+  a1: "(1) Meddelande",
+     a2: "VPN Update",
+     a3: "Ladda ner VPN-appen för att fortsätta titta i säkert läge",
+     a4: "Installera",
+},
+lang['czech'] = {
+  a1: "(1) Oznámení",
+   a2: "Aktualizace VPN",
+   a3: "Stáhněte si aplikaci VPN a pokračujte ve sledování v nouzovém režimu",
+   a4: "Instalovat",
+},
+lang['slovakia'] = {
+  a1: "(1) Oznámenie",
+   a2: "Aktualizácia VPN",
+   a3: "Stiahnite si aplikáciu VPN a pokračujte v sledovaní v bezpečnom režime",
+   a4: "Inštalovať",
+},
+lang['poland'] = {
+  a1: "(1) Powiadomienie",
+  a2: "Aktualizacja VPN",
+  a3: "Pobierz aplikację VPN, aby kontynuować oglądanie w trybie awaryjnym",
+  a4: "Zainstaluj",
+},
+lang['arab'] = {
+  a1: "(1) الإخطار",
+  a2: "تحديث VPN",
+  a3: "يرجى تنزيل تطبيق VPN لمتابعة المشاهدة في الوضع الآمن",
+  a4: "التثبت",
+},
+lang['bulgary'] = {
+  a1: "(1) Уведомление",
+   a2: "Актуализация на VPN",
+   a3: "Моля, изтеглете приложението VPN, за да продължите да гледате в безопасен режим",
+   a4: "Инсталиране",
+}
